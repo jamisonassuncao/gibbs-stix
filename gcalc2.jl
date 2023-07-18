5dad03e3f86f2f72dd1f99a9d17da0cd227fa437
Jamison Assuncao <jamison.assuncao@usp.br>
2023-07-18 17:38:31 +0200

    OOP and clean-up

--- gcalc2.jl
+++ gcalc2.jl
@@ -1,20 +1,24 @@
-seif = [-794335.4000000000,    # Helmoltz energy (F0, J/mol)
-        -3.000000000000000,    # negative of the number of atoms per formula unit (-n)
-        -1.367000000000000,    # negative of the volume (-V0)
-        3275843.0000000000,    # c1: isothermal bulk modulus (K0, bar)
-        4.0155300000000000,    # c2: pressure derivative of the isothermal bulk modulus (K')
-        1140.7720000000000,    # c3: Debye Temperature (Θ0, K)
-        1.3746600000000000,    # c4: Gruneisen thermal parameter (γ0)
-        2.8351700000000000,    # c5: Mie-Gruneisen exponent (q0)
-        4.9710780000000000,    # c6: Shear strain derivative of the tensorial Gruneisen parameter (ηS0)
-        0.0000000000000000]    # c7: Configurational (and magnetic) entropy (J/mol/K)
+struct Phase
+    id::String      # Name id
+    F0::Float64     # Helmoltz energy (F0, J/mol)
+    n::Float64      # negative of the number of atoms per formula unit (-n)
+    V0::Float64     # negative of the volume (-V0)
+    K0::Float64     # c1: isothermal bulk modulus (K0, bar)
+    Kp::Float64     # c2: pressure derivative of the isothermal bulk modulus (K')
+    Θ0::Float64     # c3: Debye Temperature (Θ0, K)
+    γ0::Float64     # c4: Gruneisen thermal parameter (γ0)
+    q0::Float64     # c5: Mie-Gruneisen exponent (q0)
+    ηS0::Float64    # c6: Shear strain derivative of the tensorial Gruneisen parameter (ηS0)
+    cme::Float64    # c7: Configurational (and magnetic) entropy (J/mol/K)
+end
 
-coe = [-855068.5, -3, -2.065700, 1135856.0, 4.00000, 852.4267, 0.3915700, 1.00000, 2.39793, 0.0]
-q   = [-858853.4, -3, -2.367003, 495474.30, 4.33155, 816.3307, -0.296e-2, 1.00000, 2.36469, 0.0]
-st  = [-818984.6, -3, -1.401700, 3143352.0, 3.75122, 1107.824, 1.3746600, 2.83517, 4.60904, 0.0]
+seif    = Phase("seif", -794335.4, -3, -1.367000, 3275843.0, 4.01553, 1140.772, 1.3746600, 2.83517, 4.971078, 0.0)
+coe     = Phase( "coe", -855068.5, -3, -2.065700, 1135856.0, 4.00000, 852.4267, 0.3915700, 1.00000, 2.397930, 0.0)
+q       = Phase(   "q", -858853.4, -3, -2.367003, 495474.30, 4.33155, 816.3307, -0.296e-2, 1.00000, 2.364690, 0.0)
+st      = Phase(  "st", -818984.6, -3, -1.401700, 3143352.0, 3.75122, 1107.824, 1.3746600, 2.83517, 4.609040, 0.0)
 # cp(pr,t) = c1 + c2*t + c3/(t*t) + c4*t*t + c5/t**(1/2) + c6/t + c7 /t**3
+
 global R = 8.31446261815324
-println("Starting...")
 
 function plg(t)
     # -----------------------------------------------------------------------
@@ -22,8 +26,8 @@ function plg(t)
     # -----------------------------------------------------------------------
     p0 = exp(-t)
     p1 = 1.0
-    p2 = t*t
-    p3 = 2.0*t
+    p2 = t * t
+    p3 = 2.0 * t
     nopt50 = 4.7637509757004091e-015
     # c 45/Pi
     plg = -2.1646464674222763831
@@ -33,68 +37,71 @@ function plg(t)
 
         p4 = Float64(i)
         p1 = p1 * p0
-        dinc = (p2 + (p3 + 2.0/p4)/p4)*p1/p4/p4
+        dinc = (p2 + (p3 + 2.0 / p4) / p4) * p1 / p4 / p4
         plg = plg + dinc
 
-        if (abs(dinc/(1.0+abs(plg))) < nopt50) 
+        if (abs(dinc / (1.0 + abs(plg))) < nopt50)
             return plg
         end
 
         i += 1
-    end 
+    end
     return plg
 end
 
-function gcalc(t=1000.0, p=1000.0, thermo=seif)
+function gcalc(t=1000.0, p=1000.0, phase=q)
+    println(repeat("=", 80))
+    println("Calculating Gibbs energy for <", phase.id, ">")
+    tr = 300.0
 
-    tr = 300.0                                          # K
-
-    v0     = -thermo[3]
-    nr9    = -9.0*thermo[2]*R                           #thermo[11]
-    nr9t0  = nr9*tr                                     #thermo[20]
-    c1     = -9.0*thermo[3]*thermo[4]                   #thermo[12]
-    c2     = thermo[5]/2.0-2.0                          #thermo[13]
-    c3     = 3.0*c1*c2                                  #thermo[14]
-    aii    = 6.0*thermo[7]                              #thermo[15]
-    aiikk2 = 0.5*aii*(-2.0+6.0*thermo[7]-3.0*thermo[8]) #thermo[18]
-    aii2   = 3*thermo[7]                                #thermo[19]
+    v0 = -phase.V0
+    nr9 = -9.0 * phase.n * R
+    nr9t0 = nr9 * tr
+    c1 = -9.0 * phase.V0 * phase.K0
+    c2 = phase.Kp / 2.0 - 2.0
+    c3 = 3.0 * c1 * c2
+    aii = 6.0 * phase.γ0
+    aiikk2 = 0.5 * aii * (-2.0 + 6.0 * phase.γ0 - 3.0 * phase.q0)
+    aii2 = 3 * phase.γ0
 
     # aiikk  = thermo[16]
 
-    r23 = 2.0/3.0
-    r59 = 5.0/9.0
+    r23 = 2.0 / 3.0
+    r59 = 5.0 / 9.0
 
     iopt21 = 100
     nopt51 = 1.8590370495272219e-13
 
-    t1     = thermo[6]/t
-    t2     = t/tr
-    nr9t   = nr9*t
+    t1 = phase.Θ0 / t
+    t2 = t / tr
+    nr9t = nr9 * t
     # initial guess for volume:
     #   taylor(diff(FTH,v),v=v0,1)
     #   JADC Feb 26, 2008.
     #   the dfth0 could be loaded as a constant.
-    tht    = t1
-    tht0   = tht*t2
+    tht = t1
+    tht0 = tht * t2
 
-    k00    = thermo[4]
-    k0p    = thermo[5]
-    gamma0 = thermo[7]
+    k00 = phase.K0
+    k0p = phase.Kp
+    γ0 = phase.γ0
 
-    dfth   = nr9t*gamma0/v0*(3.0*plg(tht)/tht^3 - log(1.0-exp(-tht)))
-    dfth0  = nr9t0*gamma0/v0*(3.0*plg(tht0)/tht0^3 - log(1.0-exp(-tht0)))
+    dfth = nr9t * γ0 / v0 * (3.0 * plg(tht) / tht^3 - log(1.0 - exp(-tht)))
+    dfth0 = nr9t0 * γ0 / v0 * (3.0 * plg(tht0) / tht0^3 - log(1.0 - exp(-tht0)))
     #  taylor(diff(FC,v),v=v0,2)
     #       v = (k00-dfth+dfth0-p)/k00*v0
     #  taylor(diff(FC,v),v=v0,3)
-    root = k00*((2.0+2.0*k0p)*(p+dfth-dfth0) + k00)
+    root = k00 * ((2.0 + 2.0 * k0p) * (p + dfth - dfth0) + k00)
 
     v = 0.0
-    if (root > 0.0) 
-        v = ((2.0+k0p)-sqrt(root)/k00)*v0/(1.0+k0p)
-        if (v < v0/10.0) || (v > v0*10.0) v = v0 end
+    if (root > 0.0)
+        v = ((2.0 + k0p) - sqrt(root) / k00) * v0 / (1.0 + k0p)
+        if (v < v0 / 10.0) || (v > v0 * 10.0)
+            v = v0
+        end
     else
         v = v0
-    end 
+    end
 
     itic = 0
     ibad = 4
@@ -103,76 +110,83 @@ function gcalc(t=1000.0, p=1000.0, thermo=seif)
     while (itic < 100)
 
         itic += 1
-        println("itic: ", itic)
+        # println("itic: ", itic)
         #  f, and derivatives
-        v23 = (v0/v)^r23
-        f = 0.5*v23 - 0.5
-        df = -v23/v/3.0
-        d2f = r59*v23/v^2
+        v23 = (v0 / v)^r23
+        f = 0.5 * v23 - 0.5
+        df = -v23 / v / 3.0
+        d2f = r59 * v23 / v^2
         # cold part derivatives
-        dfc = (c3*f+c1)*f*df
-        d2fc = (2.0*c3*f+c1)*df^2+(c3*f+c1)*f*d2f
+        dfc = (c3 * f + c1) * f * df
+        d2fc = (2.0 * c3 * f + c1) * df^2 + (c3 * f + c1) * f * d2f
         # debye T/T (tht)
-        z = 1.0+(aii+aiikk2*f)*f
+        z = 1.0 + (aii + aiikk2 * f) * f
 
-        if (z < 0.0 || v/v0 > 100.0 || v/v0 < 1e-2) println("ERROR z or v/v0") end
+        if (z < 0.0 || v / v0 > 100.0 || v / v0 < 1e-2)
+            println("ERROR z or v/v0")
+        end
         # println(z)
 
         root = sqrt(z)
 
-        tht   = t1*root
-        tht0  =  tht*t/tr
+        tht = t1 * root
+        tht0 = tht * t / tr
         # tht derivatives
-        a2f   = aii2+aiikk2*f
-        da    = a2f/root
-        dtht  = t1*da*df
-        d2tht = t1*((aiikk2/root-a2f^2/z^1.5)*df^2 + da*d2f)
+        a2f = aii2 + aiikk2 * f
+        da = a2f / root
+        dtht = t1 * da * df
+        d2tht = t1 * ((aiikk2 / root - a2f^2 / z^1.5) * df^2 + da * d2f)
         # println(d2tht)
 
-        dtht0 = dtht*t2
-        d2tht0 = d2tht*t2
+        dtht0 = dtht * t2
+        d2tht0 = d2tht * t2
         # polylog functions:
-        fpoly   = 3.0*plg(tht)/tht^3
-        fpoly0  = 3.0*plg(tht0)/tht0^3
+        fpoly = 3.0 * plg(tht) / tht^3
+        fpoly0 = 3.0 * plg(tht0) / tht0^3
         # thermal part derivatives:
-        etht  = exp(-tht )
+        etht = exp(-tht)
 
-        if (1.0-etht < 0.0) println("ERROR 1-etht") end
+        if (1.0 - etht < 0.0)
+            println("ERROR 1-etht")
+        end
 
-        letht = log(1.0-etht)
+        letht = log(1.0 - etht)
 
-        dfth = (letht-fpoly)*nr9t*dtht/tht
-        d2fth = ((4.0*dtht^2/tht-d2tht)*(fpoly-letht) + dtht^2*etht/(1.0-etht))*nr9t/tht
+        dfth = (letht - fpoly) * nr9t * dtht / tht
+        d2fth = ((4.0 * dtht^2 / tht - d2tht) * (fpoly - letht) + dtht^2 * etht / (1.0 - etht)) * nr9t / tht
 
         etht0 = exp(-tht0)
 
-        if (1.0-etht0 < 0.0) println("ERROR 1-tht0") end
+        if (1.0 - etht0 < 0.0)
+            println("ERROR 1-tht0")
+        end
 
-        letht0 = log(1.0-etht0)
+        letht0 = log(1.0 - etht0)
 
-        dfth0 = (letht0-fpoly0)*nr9t0*dtht0/tht0
-        d2fth0 = ((4.0*dtht0^2/tht0-d2tht0)*(fpoly0-letht0) + dtht0^2*etht0/(1.0-etht0))*nr9t0/tht0
+        dfth0 = (letht0 - fpoly0) * nr9t0 * dtht0 / tht0
+        d2fth0 = ((4.0 * dtht0^2 / tht0 - d2tht0) * (fpoly0 - letht0) + dtht0^2 * etht0 / (1.0 - etht0)) * nr9t0 / tht0
 
-        f1  = -dfc - dfth + dfth0 - p
+        f1 = -dfc - dfth + dfth0 - p
         df1 = -d2fc - d2fth + d2fth0
-        dv = f1/df1
+        dv = f1 / df1
 
-        if (v - dv < 0.0) dv = v/2.0 end
+        if (v - dv < 0.0)
+            dv = v / 2.0
+        end
 
-        # println("v: ", v)
         v -= dv
 
-        if (itic > iopt21 || abs(f1) > 1e40) 
-        # allow bad result
-            if (abs(f1/p) < 0.0) 
+        if (itic > iopt21 || abs(f1) > 1e40)
+            # allow bad result
+            if (abs(f1 / p) < 0.0)
                 ibad = 5
                 println("ERROR abs(f1/p)")
             end
-        elseif (abs(dv/(1.0+v)) < nopt51)
+        elseif (abs(dv / (1.0 + v)) < nopt51)
             bad = false
             break
         end
-    end 
+    end
 
     # if (bad)
     # if we get here, failed to converge
@@ -193,38 +207,29 @@ function gcalc(t=1000.0, p=1000.0, thermo=seif)
     #       gstxgi  = 1d2*p
     #       badend(id) = .true.
     #       return
-
     #       end if
-
     # end if
-    # everything is ok, now get helmoltz energy:
-    f = 0.5*(v0/v)^r23 - 0.5
-    # println(f)
-    z = 1.0+(aii+aiikk2*f)*f
+
+    # get helmoltz energy:
+    f = 0.5 * (v0 / v)^r23 - 0.5
+    z = 1.0 + (aii + aiikk2 * f) * f
     root = sqrt(z)
     # final estimate for tht
-    tht   = t1*root
-    tht0  = tht*t2
+    tht = t1 * root
+    tht0 = tht * t2
     # helmholtz enery
-    a = thermo[1] + c1*f^2*(0.5 + c2*f) + nr9*(t/tht^3*plg(tht) -tr/tht0^3*plg(tht0))
-    println("ℱ: ", a)
-    gstxgi = a + p*v - t*thermo[10]
-    println("𝒢: ", gstxgi)
-    # # z = (theta/theta0)^2
-    # gamma = (2.0*f+1.0)*(aii+aiikk*f)/6.0/z
-    # etas = - gamma - thermo[17]/z*(2.0*f + 1.0)^2
-    # # thermal energy/V, based on previous v estimate
-    # if (gamma < 0.0)
-    #     ethv = (dfth0-dfth)/gamma
-    # else
-    #     ethv = 0.0
-    # end
-    # # adiabatic shear modulus
-    # smu = (1.0 + 2.0*f)^(2.5)*(emod[1] + f*(thermo[21] + thermo[22]*f)) - etas*ethv
+    a = phase.F0 + c1 * f^2 * (0.5 + c2 * f) + nr9 * (t / tht^3 * plg(tht) - tr / tht0^3 * plg(tht0))
+    # println("F: ", a)
+    gstxgi = a + p * v - t * phase.cme
+    println("G: ", gstxgi)
+    return gstxgi
 end
 
-gcalc(1000.0, 1000.0, seif)
-gcalc(1000.0, 1000.0, coe)
-gcalc(1000.0, 1000.0, q)
-gcalc(1000.0, 1000.0, st)
-println("Finished!")
+function Σαμ()
+
+end
+
+G = gcalc(1000.0, 1000.0, seif);
+G = gcalc(1000.0, 1000.0, coe);
+G = gcalc(1000.0, 1000.0, q);
+G = gcalc(1000.0, 1000.0, st);
