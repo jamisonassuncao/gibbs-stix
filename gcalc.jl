cafb4f88cb2db05ed7425fa5040184838fd829ef
Jamison Assunção <jamison.assuncao@gmail.com>
2023-08-23 10:02:22 +0200

    cleaning up

--- gcalc.jl
+++ gcalc.jl
@@ -1,243 +1,276 @@
+using CSV
+using DataFrames
+using Printf
+
+global R = 8.31446261815324
+
+struct Phase
+    id::String      # Name id
+    comp1::String   # Composition
+    comp2::String   # Composition
+    F0::Float64     # Helmoltz energy (F0, J/mol)
+    n::Float64      # negative of the number of atoms per formula unit (-n)
+    V0::Float64     # negative of the volume (-V0)
+    K0::Float64     # c1: isothermal bulk modulus (K0, bar)
+    Kp::Float64     # c2: pressure derivative of the isothermal bulk modulus (K')
+    Θ0::Float64     # c3: Debye Temperature (Θ0, K)
+    γ0::Float64     # c4: Gruneisen thermal parameter (γ0)
+    q0::Float64     # c5: Mie-Gruneisen exponent (q0)
+    ηS0::Float64    # c6: Shear strain derivative of the tensorial Gruneisen parameter (ηS0)
+    cme::Float64    # c7: Configurational (and magnetic) entropy (J/mol/K)
+end
+# cp(pr,t) = c1 + c2*t + c3/(t*t) + c4*t*t + c5/t**(1/2) + c6/t + c7 /t**3
+
+# seif    = Phase("seif", -794335.4, -3, -1.367000, 3275843.0, 4.01553, 1140.772, 1.3746600, 2.83517, 4.971078, 0.0)
+# coe     = Phase( "coe", -855068.5, -3, -2.065700, 1135856.0, 4.00000, 852.4267, 0.3915700, 1.00000, 2.397930, 0.0)
+# q       = Phase(   "q", -858853.4, -3, -2.367003, 495474.30, 4.33155, 816.3307, -0.296e-2, 1.00000, 2.364690, 0.0)
+# st      = Phase(  "st", -818984.6, -3, -1.401700, 3143352.0, 3.75122, 1107.824, 1.3746600, 2.83517, 4.609040, 0.0)
+
+"""
+    plg(t)
+
+This function evaluates the Debye integral:
+    int((ln(1-exp(-t))*t^2),t=0..t)
+
+# Arguments
+- `t::Float64`: Temperature.
+
+# Returns
+- `plg::Float64`: Value of the integral
+"""
 function plg(t)
-    # 29th order series expansion of polylog terms in sixtrude's EoS about
-    # t = 0, good to 0.0001 accuracy to t = 5.
-    # f := int((ln(1-exp(-t))*t^2),t=0..TD);
-    # gg := convert(series(f,TD=0,29),polynom);
-    # see equation 4 in Stixrude 1990
+    p0 = exp(-t)
+    p1 = 1.0
+    p2 = t * t
+    p3 = 2.0 * t
+    nopt50 = 4.7637509757004091e-015
+    # c 45/Pi
+    plg = -2.1646464674222763831
 
-    c2 = 0.1111111111111111111
+    i = 1
+    while i < 100000
 
-    plg = t^3.0 * ((log(t) / 3.0 - c2) - t / 8.0 + t^2.0 / 120.0 \
-                    -t^4 / 20160.0 + t^6 / 1632960.0 - t^8 / 106444800.0 \
-                    +t^10 / 6227020800.0 - 0.2935661189E-11 * t^12 \
-                    +0.562291451E-13 * t^14 - 0.1115026413E-14 * t^16 \
-                    +0.2271444989E-16 * t^18 - 0.4727975432E-18 * t^20 \
-                    +0.1001636878E-19 * t^22 - 0.2153466772E-21 * t^24)
+        p4 = Float64(i)
+        p1 = p1 * p0
+        dinc = (p2 + (p3 + 2.0 / p4) / p4) * p1 / p4 / p4
+        plg = plg + dinc
 
+        if (abs(dinc / (1.0 + abs(plg))) < nopt50)
+            return plg
+        end
+
+        i += 1
+    end
     return plg
 end
 
-function print1000(v1, v2, str1)
-    println("**warning** failed to converge at ")
-    println("T= ", round(v1, digits=2), " K ")
-    println("P= ", round(v2, digits=1), " bar")
-    println("Using Stixrude EoS.")
-    println("Phase ", str1, " will be destabilized.")
-end
+"""
+    gcalc(t, p, phase)
 
-function print1010()
-    println("#" ^ 80)
-    println("# **warnign** will not be repeated for future ")
-    println("# instances of this problem")
-    println("#" ^ 80)
-end
+This function calculates the Gibbs energy of the `phase`
 
-function check_error(IZAP, NAME, T, P)
-    if IZAP < 100
-        println("**error**")
-        print1000(T, P, NAME)
-    elseif IZAP == 100 
-        print1010()
-    end 
-    IZAP += 1
-    𝒢 = 1.0e30
-    return IZAP, 𝒢
-end
+# Arguments
+- `t::Float64`: Temperature value in K.
+- `p::Float64`: Pressure value in bar.
+- `phase::Phase`: Phase object containing the phase data.
 
-function gcalc(T=1000.0, P=1000.0, 
-    # T0=300.0,
-    # H0R=-860.11803, 
-    # NN0=1.0, 
-    # VAA=5.76, 
-    # K0=614.2537, 
-    # K01=197.8011, 
-    # V0R=22.421050, 
-    # γ0=-0.03958, 
-    # q0=1.0,
-    # θ0=884.20481)
-    T0=300.0,
-    H0R=-794335.40, 
-    NN0=3.0, 
-    VAA=4.97107, 
-    K0=3275843.0, 
-    K01=4.015530, 
-    V0R=1.367, 
-    γ0=-1.374660, 
-    q0=2.835170,
-    θ0=1140.77199)
-    """
-    Volume function from Stixrude and Lithgow-Bertelloni 2011
+# Returns
+- `G::Float64`: Gibbs energy value.
+"""
+function gcalc(t=1000.0, p=1000.0, phase=q)
+    println("Calculating Gibbs energy for `", phase.id, "` (", phase.comp2, ")")
+    tr = 300.0
 
-    # Arguments                                                     -> .f90 arguments
-    'H0R': Helmoltz energy (F0, J/mol)                              -> H0R
-    'NN0': number of atoms per formula unit (n)                     -> NAT & NN0
-    'VAA': Configurations (and magnetic) entropy (J/mol/K)          -> VAA
-    'K0' : isothermal bulk modulus (K0, kbar)                       -> VPA & K0
-    'K01': pressure derivative of the isothermal bulk modulus (K')  -> VPB & K01
-    'V0R': reference volume (V0)                                    -> V0R
-    'γ0' : Gruneisen thermal paramenter (γ0)                        -> D2
-    'q0' : Mie-Gruneisen exponent (q0)                              -> D3
-    'θ0' : Debye Temperature (θ0, K)                                -> D1
-    """
+    v0 = -phase.V0
+    nr9 = -9.0 * phase.n * R
+    nr9t0 = nr9 * tr
+    c1 = -9.0 * phase.V0 * phase.K0
+    c2 = phase.Kp / 2.0 - 2.0
+    c3 = 3.0 * c1 * c2
+    aii = 6.0 * phase.γ0
+    aiikk2 = 0.5 * aii * (-2.0 + 6.0 * phase.γ0 - 3.0 * phase.q0)
+    aii2 = 3 * phase.γ0
 
-    # The value of F0 is:   -794335.40000000002     
-    # The value of n is:   -3.0000000000000000     
-    # The value of v0 is:   -1.3670000000000000     
-    # The value of k0 is:    3275843.0000000000     
-    # The value of k0p is:    4.0155300000000000     
-    # The value of theta0 is:    1140.7719999999999     
-    # The value of gamma0 is:    1.3746600000000000     
-    # The value of q is:    2.8351700000000002     
-    # The value of etaS0 is:    4.9710799999999997     
-    # The value of Smag is:    0.0000000000000000
+    # aiikk  = thermo[16]
 
-    IZAP = 0
+    r23 = 2.0 / 3.0
+    r59 = 5.0 / 9.0
 
-    # initial parameters
-    R = 8.31446261815324 # Gas constant (J/(mol K))
-    NR9 = 9.0 * NN0 * R # 9nR
-    C1 = 9.0 * K0 * V0R # 9K₀V₀
-    C2 = K01 / 2.0 - 2.0 # K'/2 - 2
-    C3 = 3.0 * C1 * C2
-    C5 = 3.0 * γ0 # 3γ₀
-    C7 = C5 * (-2.0 + 6.0 * γ0 - 3.0 * q0)
-    QM1 = q0 - 1.0
-    DT = T - T0
+    iopt21 = 100
+    nopt51 = 1.8590370495272219e-13
 
-    # 'Newton–Raphson'-method approximation:
-    #   initial guess for volume uses a 4 order expansion on the polylog terms 
-    #   before differentiation and a second order series on the resulting 
-    #   derivative.
-    VOLUM = V0R * (3.0 * ((2.0 - q0) * DT * γ0 * NR9 - 3.0 * P * V0R) + C1) / (C1 - QM1 * DT * γ0 * NR9)
-    # VOLUM = V0R * (3.0*((2.0-q0)*DT*γ0*NR9 - 3.0*P*V0R) + C1) / 
-    # (C1 - QM1 * DT * γ0 * NR9)
-    if (VOLUM < 0.0) VOLUM = V0R end
+    t1 = phase.Θ0 / t
+    t2 = t / tr
+    nr9t = nr9 * t
+    # initial guess for volume:
+    #   taylor(diff(FTH,v),v=v0,1)
+    #   JADC Feb 26, 2008.
+    #   the dfth0 could be loaded as a constant.
+    tht = t1
+    tht0 = tht * t2
 
-    # run 'Newton-Raphson'-method approximation
-    ITIC = 0
-    FL = 1.0e9 # (bar)
-    TOL = 1.0e-6 * P # (bar)
+    k00 = phase.K0
+    k0p = phase.Kp
+    γ0 = phase.γ0
 
-    ROOT_ER = 0
-    ETHT_ER = 0
-    VOLU_ER = 0
-    ITIC_ER = 0
-    FL_ER   = 0
+    dfth = nr9t * γ0 / v0 * (3.0 * plg(tht) / tht^3 - log(1.0 - exp(-tht)))
+    dfth0 = nr9t0 * γ0 / v0 * (3.0 * plg(tht0) / tht0^3 - log(1.0 - exp(-tht0)))
+    #  taylor(diff(FC,v),v=v0,2)
+    #       v = (k00-dfth+dfth0-p)/k00*v0
+    #  taylor(diff(FC,v),v=v0,3)
+    root = k00 * ((2.0 + 2.0 * k0p) * (p + dfth - dfth0) + k00)
 
-    while (abs(FL) > TOL)
-        ITIC += 1
-        VQ = (VOLUM / V0R)^q0
-        GVQ = γ0 * VQ
-        # Birch-Murnaghan finite strain
-        V23 = (V0R / VOLUM)^(2.0/3.0)           # -> eq. 16 @ https://www.perplex.ethz.ch/perplex_thermodynamic_data_file.html
-        F = 0.5 * V23 - 0.5                     # -> eq. 16 @ https://www.perplex.ethz.ch/perplex_thermodynamic_data_file.html
-        DF = -V23 / VOLUM / 3.0
-        D2F = 5.0 / 9.0 * V23 / VOLUM^2.0
-        # Debye T/T 
-        ROOT = sqrt(1.0 + C7 * F^2.0 + 2.0 * C5 * F) # eq. 21 @ https://www.perplex.ethz.ch/perplex_thermodynamic_data_file.html
-        if (ROOT^2.0 < 0.0)
-            ROOT_ER += 1
-            IZAP, 𝒢 = check_error(IZAP, :ROOT, T, P)
+    v = 0.0
+    if (root > 0.0)
+        v = ((2.0 + k0p) - sqrt(root) / k00) * v0 / (1.0 + k0p)
+        if (v < v0 / 10.0) || (v > v0 * 10.0)
+            v = v0
+        end
+    else
+        v = v0
+    end
+
+    itic = 0
+    ibad = 4
+    bad = true
+
+    while (itic < 100)
+
+        itic += 1
+        # println("itic: ", itic)
+        #  f, and derivatives
+        v23 = (v0 / v)^r23
+        f = 0.5 * v23 - 0.5
+        df = -v23 / v / 3.0
+        d2f = r59 * v23 / v^2
+        # cold part derivatives
+        dfc = (c3 * f + c1) * f * df
+        d2fc = (2.0 * c3 * f + c1) * df^2 + (c3 * f + c1) * f * d2f
+        # debye T/T (tht)
+        z = 1.0 + (aii + aiikk2 * f) * f
+
+        if (z < 0.0 || v / v0 > 100.0 || v / v0 < 1e-2)
+            println("ERROR z or v/v0")
+        end
+        # println(z)
+
+        root = sqrt(z)
+
+        tht = t1 * root
+        tht0 = tht * t / tr
+        # tht derivatives
+        a2f = aii2 + aiikk2 * f
+        da = a2f / root
+        dtht = t1 * da * df
+        d2tht = t1 * ((aiikk2 / root - a2f^2 / z^1.5) * df^2 + da * d2f)
+        # println(d2tht)
+
+        dtht0 = dtht * t2
+        d2tht0 = d2tht * t2
+        # polylog functions:
+        fpoly = 3.0 * plg(tht) / tht^3
+        fpoly0 = 3.0 * plg(tht0) / tht0^3
+        # thermal part derivatives:
+        etht = exp(-tht)
+
+        if (1.0 - etht < 0.0)
+            println("ERROR 1-etht")
         end
 
-        THT = θ0 / T * ROOT
-        THT0 = THT * T / T0
+        letht = log(1.0 - etht)
 
-        ETHT = exp(-THT)
-        if (1-ETHT < 0.0)
-            ETHT_ER += 1
-            IZAP, 𝒢 = check_error(IZAP, :ETHT, T, P)
+        dfth = (letht - fpoly) * nr9t * dtht / tht
+        d2fth = ((4.0 * dtht^2 / tht - d2tht) * (fpoly - letht) + dtht^2 * etht / (1.0 - etht)) * nr9t / tht
+
+        etht0 = exp(-tht0)
+
+        if (1.0 - etht0 < 0.0)
+            println("ERROR 1-tht0")
         end
-        ETHT0 = exp(-THT0)
-        LTHT = log(1.0 - ETHT)
-        LTHT0 = log(1.0 - ETHT0)
-        # diff(theta/T,v)
-        DD2 = (C7 * F + C5) / ROOT
-        DD1 = θ0 * DF * DD2
-        DTHT = DD1 / T
-        DTHT0 = DD1 / T0
-        # diff(theta/T,v,v)
-        DD3 = θ0 * (DF^2.0 * (C7 - C5^2.0) / ROOT^3.0 + D2F * DD2)
-        D2THT = DD3 / T
-        D2THT0 = DD3 / T0
 
-        PLGG = plg(THT)
-        PLGG0 = plg(THT0)
-        DG = THT^2.0 * LTHT * DTHT
-        DG0 = THT0^2.0 * LTHT0 * DTHT0
-        D2G = ((2.0 * LTHT + THT * ETHT / (1.0 - ETHT)) * DTHT^2 \
-               +THT * LTHT * D2THT) * THT
-        D2G0 = ((2.0 * LTHT0 + THT0 * ETHT0 / (1.0 - ETHT0)) * DTHT0^2 \
-                +THT0 * LTHT0 * D2THT0) * THT0
+        letht0 = log(1.0 - etht0)
 
-        DFC = (C3 * F + C1) * F * DF
-        D2FC = (2.0 * C3 * F + C1) * DF^2 + (C3 * F + C1) * F * D2F
+        dfth0 = (letht0 - fpoly0) * nr9t0 * dtht0 / tht0
+        d2fth0 = ((4.0 * dtht0^2 / tht0 - d2tht0) * (fpoly0 - letht0) + dtht0^2 * etht0 / (1.0 - etht0)) * nr9t0 / tht0
 
-        DFT = NR9 * T / THT^3 * (DG - 3.0 / THT * PLGG * DTHT)
-        DFT0 = NR9 * T0 / THT0^3 * (DG0 - 3.0 / THT0 * PLGG0 * DTHT0)
+        f1 = -dfc - dfth + dfth0 - p
+        df1 = -d2fc - d2fth + d2fth0
+        dv = f1 / df1
 
-        D2FT = NR9 * T / THT^3 * (3.0 / THT * (DTHT * (4.0 / THT * PLGG * DTHT \
-                                                       -2.0 * DG) - PLGG * D2THT) + D2G)
-        D2FT0 = NR9 * T0 / THT0^3 * (3.0 / THT0 * (DTHT0 * (4.0 / THT0 * PLGG0 * DTHT0 \
-                                                            -2.0 * DG0) - PLGG0 * D2THT0) + D2G0)
+        if (v - dv < 0.0)
+            dv = v / 2.0
+        end
 
-        FL  = - DFC - DFT + DFT0 - P   # P(V) bar
-        # println(ITIC, ": ", FL)
+        v -= dv
 
-        DFL = - D2FC - D2FT + D2FT0    # dP(V)/dV
-        #'Newton-Raphson'-method
-        VOLUM -= FL / DFL
-        if (VOLUM <= 0.0 || ITIC > 40 || abs(FL) > 1.0e40)
-            if (VOLUM <= 0) VOLU_ER += 1 end
-            if (ITIC > 0) ITIC_ER += 1 end
-            if (FL <=0) FL_ER += 1 end
-            IZAP, 𝒢 = check_error(IZAP, :VOLUM, T, P)
+        if (itic > iopt21 || abs(f1) > 1e40)
+            # allow bad result
+            if (abs(f1 / p) < 0.0)
+                ibad = 5
+                println("ERROR abs(f1/p)")
+            end
+        elseif (abs(dv / (1.0 + v)) < nopt51)
+            bad = false
+            break
         end
     end
 
-    VQ = (VOLUM / V0R)^q0
-    # Volume (Birch-Murnaghan finite strain)
-    F = 0.5 * (V0R / VOLUM)^(2.0/3.0) - 0.5
-    ROOT = sqrt(1.0 + C7 * F^2.0 + 2.0 * C5 * F)
-    THT = θ0 / T * ROOT
-    THT0 = THT * T / T0
+    # if (bad)
+    # if we get here, failed to converge
+    #       if (izap.le.iopt(1)) then
 
-    # Helmholtz energy
-    ℱ = H0R + C1 * F^2.0 * (0.5 + C2 * F) + NR9 * (T / THT^3 * plg(THT) - T0 / THT0^3.0 * plg(THT0))
-    # ℱ = H0R + 
-    # C1 * F^2.0 * (0.5 + C2 * F) + 
-    # NR9 * (T / THT^3 * plg(THT) - 
-    # T0 / THT0^3.0 * plg(THT0))
-    # Legendre transformation
-    𝒢 = ℱ + P * VOLUM - T * VAA
-    println("# ROOT errors: ", ROOT_ER)
-    println("# ETHT errors: ", ETHT_ER)
-    println("# VOLU errors: ", VOLU_ER)
-    println("# ITIC errors: ", ITIC_ER)
-    println("# FL errors: ", FL_ER)
+    #       msg = 'STXGJI/'//names(id)
 
-    println("ℱ: ", ℱ)
-    println("𝒢: ", 𝒢)
+    #       call conwrn (ibad,msg)
 
-    return 𝒢
+    #       izap = izap + 1
+
+    #       if (izap.eq.iopt(1)) call warn (49,r,93,'STXGJI')
+
+    #       end if
+
+    #       if (ibad.eq.4) then 
+    # # c                                 destabilize the phase.
+    #       gstxgi  = 1d2*p
+    #       badend(id) = .true.
+    #       return
+    #       end if
+    # end if
+
+    # get helmoltz energy:
+    f = 0.5 * (v0 / v)^r23 - 0.5
+    z = 1.0 + (aii + aiikk2 * f) * f
+    root = sqrt(z)
+    # final estimate for tht
+    tht = t1 * root
+    tht0 = tht * t2
+    # helmholtz energy
+    a = phase.F0 + c1 * f^2 * (0.5 + c2 * f) + nr9 * (t / tht^3 * plg(tht) - tr / tht0^3 * plg(tht0))
+    # println("F: ", a)
+    G = a + p * v - t * phase.cme
+    @printf("G: %10.2f\n", float(G));
+    println(repeat("=", 40))
+    return G
 end
 
-println("Starting...")
-𝒢 = gcalc()
+function gibbs(id)
+    # load data
+    data = CSV.File("stx11.csv", header=1) |> DataFrame
+    row = findfirst(data.id .== id)
+    phase = Phase(
+        data[row, :id],
+        data[row, :comp1],
+        data[row, :comp2],
+        data[row, :G0],
+        data[row, :S0],
+        data[row, :V0],
+        data[row, :c1],
+        data[row, :c2],
+        data[row, :c3],
+        data[row, :c4],
+        data[row, :c5],
+        data[row, :c6],
+        data[row, :c7])
 
-println("Done!")
-
-# if (VO2) 
-#     FVVOL = V0R * exp(VAA * (T - T0) + VAB * (TT - TT0) / 2.0)
-#     if (VB > 0.0) 
-#         FGVOL=(FVVOL / VB) * (1.0 - exp(- VB * (P - P0)))
-#         GR = GR + FGVOL
-#         VOLUM = FVVOL * DEXP(- VB * (P - P0))
-#         FVVOL = VOLUM - V0R
-#     else
-#         FGVOL = FVVOL * (P - P0)
-#         GR = GR + FGVOL
-#         VOLUM = FVVOL
-#         FVVOL = VOLUM - V0R
-#     end
-# end
+    G = gcalc(1000.0, 1000.0, phase);
+end
