10d31de8abb3b4253f9e33186e7a0b391a90f077
Jamison Assunção <jamison.assuncao@gmail.com>
2023-08-30 22:27:16 +0200

    dirt backup

---
+++ functions.jl
@@ -0,0 +1,271 @@
+using JSON
+using DataFrames
+using Printf
+
+global R = 8.31446261815324
+
+cmp = ["SIO2", "MGO", "FEO", "CAO", "AL2O3", "NA2O"]
+n_cmp = zeros(size(cmp))
+
+struct Phase
+    id::String                  # Name id
+    fml::String                 # Chemical formula
+    cmp::Dict{String, Float64}  # Chemical components (SiO2, MgO, FeO, CaO, Al2O3, Na2O)
+    F0::Float64                 # Helmoltz energy (F0, J/mol)
+    n::Float64                  # negative of the number of atoms per formula unit (-n)
+    V0::Float64                 # negative of the volume (-V0)
+    K0::Float64                 # c1: isothermal bulk modulus (K0, bar)
+    Kp::Float64                 # c2: pressure derivative of the isothermal bulk modulus (K')
+    Θ0::Float64                 # c3: Debye Temperature (Θ0, K)
+    γ0::Float64                 # c4: Gruneisen thermal parameter (γ0)
+    q0::Float64                 # c5: Mie-Gruneisen exponent (q0)
+    ηS0::Float64                # c6: Shear strain derivative of the tensorial Gruneisen parameter (ηS0)
+    cme::Float64                # c7: Configurational (and magnetic) entropy (J/mol/K)
+end
+# cp(pr,t) = c1 + c2*t + c3/(t*t) + c4*t*t + c5/t**(1/2) + c6/t + c7 /t**3
+
+struct Model
+    name::String
+    phases::Vector{String}
+    sites::Int
+end
+
+"""
+    plg(t)
+
+This function evaluates the Debye integral:
+    int((ln(1-exp(-t))*t^2),t=0..t)
+
+# Arguments
+- `t::Float64`: Temperature.
+
+# Returns
+- `plg::Float64`: Value of the integral
+"""
+function plg(t)
+    p0 = exp(-t)
+    p1 = 1.0
+    p2 = t * t
+    p3 = 2.0 * t
+    nopt50 = 4.7637509757004091e-015
+    # c 45/Pi
+    plg = -2.1646464674222763831
+
+    i = 1
+    while i < 100000
+
+        p4 = Float64(i)
+        p1 = p1 * p0
+        dinc = (p2 + (p3 + 2.0 / p4) / p4) * p1 / p4 / p4
+        plg = plg + dinc
+
+        if (abs(dinc / (1.0 + abs(plg))) < nopt50)
+            return plg
+        end
+
+        i += 1
+    end
+    return plg
+end
+
+"""
+    gibbs(t, p, phase)
+
+This function calculates the Gibbs energy of the `phase`
+
+# Arguments
+- `phase::Phase`: Phase object containing the phase data.
+- `t::Float64`: Temperature value in K.
+- `p::Float64`: Pressure value in bar.
+
+# Returns
+- `G::Float64`: Gibbs energy value.
+"""
+function gibbs(phase, t=1000.0, p=1000.0)
+    println("Calculating Gibbs energy for `", phase["id"], "` (", phase["fml"], ")")
+    tr = 300.0
+
+    v0 = -phase["V0"]
+    nr9 = -9.0 * phase["n"] * R
+    nr9t0 = nr9 * tr
+    c1 = -9.0 * phase["V0"] * phase["K0"]
+    c2 = phase["Kp"] / 2.0 - 2.0
+    c3 = 3.0 * c1 * c2
+    aii = 6.0 * phase["γ0"]
+    aiikk2 = 0.5 * aii * (-2.0 + 6.0 * phase["γ0"] - 3.0 * phase["q0"])
+    aii2 = 3 * phase["γ0"]
+
+    # aiikk  = thermo[16]
+
+    r23 = 2.0 / 3.0
+    r59 = 5.0 / 9.0
+
+    iopt21 = 100
+    nopt51 = 1.8590370495272219e-13
+
+    t1 = phase["Θ0"] / t
+    t2 = t / tr
+    nr9t = nr9 * t
+    # initial guess for volume:
+    #   taylor(diff(FTH,v),v=v0,1)
+    #   JADC Feb 26, 2008.
+    #   the dfth0 could be loaded as a constant.
+    tht = t1
+    tht0 = tht * t2
+
+    k00 = phase["K0"]
+    k0p = phase["Kp"]
+    γ0 = phase["γ0"]
+
+    dfth = nr9t * γ0 / v0 * (3.0 * plg(tht) / tht^3 - log(1.0 - exp(-tht)))
+    dfth0 = nr9t0 * γ0 / v0 * (3.0 * plg(tht0) / tht0^3 - log(1.0 - exp(-tht0)))
+    #  taylor(diff(FC,v),v=v0,2)
+    #       v = (k00-dfth+dfth0-p)/k00*v0
+    #  taylor(diff(FC,v),v=v0,3)
+    root = k00 * ((2.0 + 2.0 * k0p) * (p + dfth - dfth0) + k00)
+
+    v = 0.0
+    if (root > 0.0)
+        v = ((2.0 + k0p) - sqrt(root) / k00) * v0 / (1.0 + k0p)
+        if (v < v0 / 10.0) || (v > v0 * 10.0)
+            v = v0
+        end
+    else
+        v = v0
+    end
+
+    itic = 0
+    ibad = 4
+    bad = true
+
+    while (itic < 100)
+
+        itic += 1
+        # println("itic: ", itic)
+        #  f, and derivatives
+        v23 = (v0 / v)^r23
+        f = 0.5 * v23 - 0.5
+        df = -v23 / v / 3.0
+        d2f = r59 * v23 / v^2
+        # cold part derivatives
+        dfc = (c3 * f + c1) * f * df
+        d2fc = (2.0 * c3 * f + c1) * df^2 + (c3 * f + c1) * f * d2f
+        # debye T/T (tht)
+        z = 1.0 + (aii + aiikk2 * f) * f
+
+        if (z < 0.0 || v / v0 > 100.0 || v / v0 < 1e-2)
+            println("ERROR z or v/v0")
+        end
+        # println(z)
+
+        root = sqrt(z)
+
+        tht = t1 * root
+        tht0 = tht * t / tr
+        # tht derivatives
+        a2f = aii2 + aiikk2 * f
+        da = a2f / root
+        dtht = t1 * da * df
+        d2tht = t1 * ((aiikk2 / root - a2f^2 / z^1.5) * df^2 + da * d2f)
+        # println(d2tht)
+
+        dtht0 = dtht * t2
+        d2tht0 = d2tht * t2
+        # polylog functions:
+        fpoly = 3.0 * plg(tht) / tht^3
+        fpoly0 = 3.0 * plg(tht0) / tht0^3
+        # thermal part derivatives:
+        etht = exp(-tht)
+
+        if (1.0 - etht < 0.0)
+            println("ERROR 1-etht")
+        end
+
+        letht = log(1.0 - etht)
+
+        dfth = (letht - fpoly) * nr9t * dtht / tht
+        d2fth = ((4.0 * dtht^2 / tht - d2tht) * (fpoly - letht) + dtht^2 * etht / (1.0 - etht)) * nr9t / tht
+
+        etht0 = exp(-tht0)
+
+        if (1.0 - etht0 < 0.0)
+            println("ERROR 1-tht0")
+        end
+
+        letht0 = log(1.0 - etht0)
+
+        dfth0 = (letht0 - fpoly0) * nr9t0 * dtht0 / tht0
+        d2fth0 = ((4.0 * dtht0^2 / tht0 - d2tht0) * (fpoly0 - letht0) + dtht0^2 * etht0 / (1.0 - etht0)) * nr9t0 / tht0
+
+        f1 = -dfc - dfth + dfth0 - p
+        df1 = -d2fc - d2fth + d2fth0
+        dv = f1 / df1
+
+        if (v - dv < 0.0)
+            dv = v / 2.0
+        end
+
+        v -= dv
+
+        if (itic > iopt21 || abs(f1) > 1e40)
+            # allow bad result
+            if (abs(f1 / p) < 0.0)
+                ibad = 5
+                println("ERROR abs(f1/p)")
+            end
+        elseif (abs(dv / (1.0 + v)) < nopt51)
+            bad = false
+            break
+        end
+    end
+
+    # if (bad)
+    # if we get here, failed to converge
+    #       if (izap.le.iopt(1)) then
+
+    #       msg = 'STXGJI/'//names(id)
+
+    #       call conwrn (ibad,msg)
+
+    #       izap = izap + 1
+
+    #       if (izap.eq.iopt(1)) call warn (49,r,93,'STXGJI')
+
+    #       end if
+
+    #       if (ibad.eq.4) then 
+    # # c                                 destabilize the phase
+    #       gstxgi  = 1d2*p
+    #       badend(id) = .true.
+    #       return
+    #       end if
+    # end if
+
+    # get helmoltz energy:
+    f = 0.5 * (v0 / v)^r23 - 0.5
+    z = 1.0 + (aii + aiikk2 * f) * f
+    root = sqrt(z)
+    # final estimate for tht
+    tht = t1 * root
+    tht0 = tht * t2
+    # helmholtz energy
+    a = phase["F0"] + c1 * f^2 * (0.5 + c2 * f) + nr9 * (t / tht^3 * plg(tht) - tr / tht0^3 * plg(tht0))
+    # println("F: ", a)
+    G = a + p * v - t * phase["cme"]
+    @printf("G(%.2f, %.2f): %10.2f\n", float(t), float(p), float(G));
+    println(repeat("=", 40))
+    return G
+end
+
+function read_model(modelname="olivine", filename="stx11_solution.json")
+    model = JSON.parsefile(filename)
+    
+    for row in model
+        if get(row, "name", nothing) == modelname
+            return row
+        end
+    end
+    
+    return nothing
+end
+    
--- gcalc.jl
+++ gcalc.jl
@@ -1,337 +1,66 @@
-using CSV
-using DataFrames
-using Printf
+using JSON
 
-global R = 8.31446261815324
+include("functions.jl")
 
-components = ["SIO2", "MGO", "FEO", "CAO", "AL2O3", "NA20"]
-n_components = zeros(size(components))
-
-struct Phase
-    id::String          # Name id
-    formula::String     # Chemical formula
-    components::Vector{Float64}  # Chemical components (SiO2, MgO, FeO, CaO, Al2O3, Na2O)
-    F0::Float64         # Helmoltz energy (F0, J/mol)
-    n::Float64          # negative of the number of atoms per formula unit (-n)
-    V0::Float64         # negative of the volume (-V0)
-    K0::Float64         # c1: isothermal bulk modulus (K0, bar)
-    Kp::Float64         # c2: pressure derivative of the isothermal bulk modulus (K')
-    Θ0::Float64         # c3: Debye Temperature (Θ0, K)
-    γ0::Float64         # c4: Gruneisen thermal parameter (γ0)
-    q0::Float64         # c5: Mie-Gruneisen exponent (q0)
-    ηS0::Float64        # c6: Shear strain derivative of the tensorial Gruneisen parameter (ηS0)
-    cme::Float64        # c7: Configurational (and magnetic) entropy (J/mol/K)
-end
-# cp(pr,t) = c1 + c2*t + c3/(t*t) + c4*t*t + c5/t**(1/2) + c6/t + c7 /t**3
-
-"""
-    plg(t)
-
-This function evaluates the Debye integral:
-    int((ln(1-exp(-t))*t^2),t=0..t)
-
-# Arguments
-- `t::Float64`: Temperature.
-
-# Returns
-- `plg::Float64`: Value of the integral
-"""
-function plg(t)
-    p0 = exp(-t)
-    p1 = 1.0
-    p2 = t * t
-    p3 = 2.0 * t
-    nopt50 = 4.7637509757004091e-015
-    # c 45/Pi
-    plg = -2.1646464674222763831
-
-    i = 1
-    while i < 100000
-
-        p4 = Float64(i)
-        p1 = p1 * p0
-        dinc = (p2 + (p3 + 2.0 / p4) / p4) * p1 / p4 / p4
-        plg = plg + dinc
-
-        if (abs(dinc / (1.0 + abs(plg))) < nopt50)
-            return plg
-        end
-
-        i += 1
-    end
-    return plg
-end
-
-"""
-    gibbs(t, p, phase)
-
-This function calculates the Gibbs energy of the `phase`
-
-# Arguments
-- `phase::Phase`: Phase object containing the phase data.
-- `t::Float64`: Temperature value in K.
-- `p::Float64`: Pressure value in bar.
-
-# Returns
-- `G::Float64`: Gibbs energy value.
-"""
-function gibbs(phase, t=1000.0, p=1000.0)
-    println("Calculating Gibbs energy for `", phase.id, "` (", phase.comp1, ")")
-    tr = 300.0
-
-    v0 = -phase.V0
-    nr9 = -9.0 * phase.n * R
-    nr9t0 = nr9 * tr
-    c1 = -9.0 * phase.V0 * phase.K0
-    c2 = phase.Kp / 2.0 - 2.0
-    c3 = 3.0 * c1 * c2
-    aii = 6.0 * phase.γ0
-    aiikk2 = 0.5 * aii * (-2.0 + 6.0 * phase.γ0 - 3.0 * phase.q0)
-    aii2 = 3 * phase.γ0
-
-    # aiikk  = thermo[16]
-
-    r23 = 2.0 / 3.0
-    r59 = 5.0 / 9.0
-
-    iopt21 = 100
-    nopt51 = 1.8590370495272219e-13
-
-    t1 = phase.Θ0 / t
-    t2 = t / tr
-    nr9t = nr9 * t
-    # initial guess for volume:
-    #   taylor(diff(FTH,v),v=v0,1)
-    #   JADC Feb 26, 2008.
-    #   the dfth0 could be loaded as a constant.
-    tht = t1
-    tht0 = tht * t2
-
-    k00 = phase.K0
-    k0p = phase.Kp
-    γ0 = phase.γ0
-
-    dfth = nr9t * γ0 / v0 * (3.0 * plg(tht) / tht^3 - log(1.0 - exp(-tht)))
-    dfth0 = nr9t0 * γ0 / v0 * (3.0 * plg(tht0) / tht0^3 - log(1.0 - exp(-tht0)))
-    #  taylor(diff(FC,v),v=v0,2)
-    #       v = (k00-dfth+dfth0-p)/k00*v0
-    #  taylor(diff(FC,v),v=v0,3)
-    root = k00 * ((2.0 + 2.0 * k0p) * (p + dfth - dfth0) + k00)
-
-    v = 0.0
-    if (root > 0.0)
-        v = ((2.0 + k0p) - sqrt(root) / k00) * v0 / (1.0 + k0p)
-        if (v < v0 / 10.0) || (v > v0 * 10.0)
-            v = v0
-        end
-    else
-        v = v0
-    end
-
-    itic = 0
-    ibad = 4
-    bad = true
-
-    while (itic < 100)
-
-        itic += 1
-        # println("itic: ", itic)
-        #  f, and derivatives
-        v23 = (v0 / v)^r23
-        f = 0.5 * v23 - 0.5
-        df = -v23 / v / 3.0
-        d2f = r59 * v23 / v^2
-        # cold part derivatives
-        dfc = (c3 * f + c1) * f * df
-        d2fc = (2.0 * c3 * f + c1) * df^2 + (c3 * f + c1) * f * d2f
-        # debye T/T (tht)
-        z = 1.0 + (aii + aiikk2 * f) * f
-
-        if (z < 0.0 || v / v0 > 100.0 || v / v0 < 1e-2)
-            println("ERROR z or v/v0")
-        end
-        # println(z)
-
-        root = sqrt(z)
-
-        tht = t1 * root
-        tht0 = tht * t / tr
-        # tht derivatives
-        a2f = aii2 + aiikk2 * f
-        da = a2f / root
-        dtht = t1 * da * df
-        d2tht = t1 * ((aiikk2 / root - a2f^2 / z^1.5) * df^2 + da * d2f)
-        # println(d2tht)
-
-        dtht0 = dtht * t2
-        d2tht0 = d2tht * t2
-        # polylog functions:
-        fpoly = 3.0 * plg(tht) / tht^3
-        fpoly0 = 3.0 * plg(tht0) / tht0^3
-        # thermal part derivatives:
-        etht = exp(-tht)
-
-        if (1.0 - etht < 0.0)
-            println("ERROR 1-etht")
-        end
-
-        letht = log(1.0 - etht)
-
-        dfth = (letht - fpoly) * nr9t * dtht / tht
-        d2fth = ((4.0 * dtht^2 / tht - d2tht) * (fpoly - letht) + dtht^2 * etht / (1.0 - etht)) * nr9t / tht
-
-        etht0 = exp(-tht0)
-
-        if (1.0 - etht0 < 0.0)
-            println("ERROR 1-tht0")
-        end
-
-        letht0 = log(1.0 - etht0)
-
-        dfth0 = (letht0 - fpoly0) * nr9t0 * dtht0 / tht0
-        d2fth0 = ((4.0 * dtht0^2 / tht0 - d2tht0) * (fpoly0 - letht0) + dtht0^2 * etht0 / (1.0 - etht0)) * nr9t0 / tht0
-
-        f1 = -dfc - dfth + dfth0 - p
-        df1 = -d2fc - d2fth + d2fth0
-        dv = f1 / df1
-
-        if (v - dv < 0.0)
-            dv = v / 2.0
-        end
-
-        v -= dv
-
-        if (itic > iopt21 || abs(f1) > 1e40)
-            # allow bad result
-            if (abs(f1 / p) < 0.0)
-                ibad = 5
-                println("ERROR abs(f1/p)")
-            end
-        elseif (abs(dv / (1.0 + v)) < nopt51)
-            bad = false
-            break
-        end
-    end
-
-    # if (bad)
-    # if we get here, failed to converge
-    #       if (izap.le.iopt(1)) then
-
-    #       msg = 'STXGJI/'//names(id)
-
-    #       call conwrn (ibad,msg)
-
-    #       izap = izap + 1
-
-    #       if (izap.eq.iopt(1)) call warn (49,r,93,'STXGJI')
-
-    #       end if
-
-    #       if (ibad.eq.4) then 
-    # # c                                 destabilize the phase.
-    #       gstxgi  = 1d2*p
-    #       badend(id) = .true.
-    #       return
-    #       end if
-    # end if
-
-    # get helmoltz energy:
-    f = 0.5 * (v0 / v)^r23 - 0.5
-    z = 1.0 + (aii + aiikk2 * f) * f
-    root = sqrt(z)
-    # final estimate for tht
-    tht = t1 * root
-    tht0 = tht * t2
-    # helmholtz energy
-    a = phase.F0 + c1 * f^2 * (0.5 + c2 * f) + nr9 * (t / tht^3 * plg(tht) - tr / tht0^3 * plg(tht0))
-    # println("F: ", a)
-    G = a + p * v - t * phase.cme
-    @printf("G(%.2f, %.2f): %10.2f\n", float(t), float(p), float(G));
-    println(repeat("=", 40))
-    return G
-end
-
-function activity(phase)
+function activity(my_comp, model)
     actvty = 0
+    n_species = size(model["species"])[1]
+    n_sites = model["sites"]
+    n_components = 1
 
-    si = 1 # number of sites
-    sp = 2 # number of species
-    co = sum([value > 0 ? 1 : 0 for value in phase.components]) # number of components
-    
-    ni = 1.0 # amount
-
-    Njk = 0.0 # number of atoms of component j on site k
-    Nk = 0.0 # total number of atoms on site k
-    Sik = 0.0 # sum over the stoichiometric coefficients of species i at site k
-
-    sijk = sum(phase.components) # stoichiometric coefficient of component j on site k in species i
-
-    for i in 1:sp
-        Njk += sijk * ni
-    end
-
-    for k in 1:si
-        for j in 1:co
-            Sik += sijk
-            Nk += Njk
+    for i in 1:n_species
+        println("specie: ", model["species"][i])
+        # G = gibbs(model["species"][i], t=1000.0, p=1000.0)
+        for k in 1:n_sites
+            for j in 1:n_components
+                println(j, k, i)
+            end
         end
-        actvty += Sik * log(Nk) - sijk * log(Njk)
     end
+
     return actvty
 end
 
-function excess()
-    excess = 0.0
-    return excess 
-end
 
-function gcalc(id, temperature=1000.0, pressure=1000.0)
-    # load data
-    data = CSV.File("stx11.csv", header=1) |> DataFrame
-    row = findfirst(data.id .== id)
+function main()
+    # load endmembers
+    species = JSON.parsefile("stx11_data.json")
+    # set components
+    my_comp = Dict("MGO" => 1.0, "SIO2" => 1.0, "FEO" => 1.0)
+    # load solution model
+    modelname = "olivine"
+    model = read_model(modelname, "stx11_solution.json")
 
-    # count components
-    comps = split(data[row, :components], r"[()]")
-    pop!(comps) # pop what is after the `)`
-    for c in 1:2:size(comps)[1]
-        p = findfirst(x -> x == comps[c], components)
-        n_components[p] = parse(Float64, comps[c+1])
+    temperature = 1000.0 # K
+    pressure = 1000.0 # bar 
+
+    n_species = size(model["species"])[1]
+    G = zeros(n_species)
+
+    i = 1
+    for specie in species
+        if specie["id"] in model["species"]
+            G[i] = gibbs(specie, 1000.0, 1000.0)
+            i += 1
+        end
     end
-
-    phase = Phase(
-        data[row, :id],
-        data[row, :formula],
-        n_components,
-        data[row, :G0],
-        data[row, :S0],
-        data[row, :V0],
-        data[row, :c1],
-        data[row, :c2],
-        data[row, :c3],
-        data[row, :c4],
-        data[row, :c5],
-        data[row, :c6],
-        data[row, :c7])
-
-    G = 0#gibbs(phase, temperature, pressure);
-    actv = R * temperature * activity(phase);
-    excs = excess()
-
-    # clean components
-    n_components .*= 0.0
-    
-    return G + actv - excs;
+    # actv = R * temperature * activity(my_comp, model);
+    return i
 end
 
-sp = 2
-ph = gcalc("fo", 1000, 1000);
-# gcalc("fa", 1000,1000);
 
-# notation
 
-# eg. Olivine
+function foo(x::Int64, y::Int64)
+    z = x + y
+    return z
+end
 
-# fo -> Mg_2Si_1O_4
-# fa -> Fe_2Si_1O_4
+function foo(x::Float64, y::Float64)
+    z = x + 2*y
+    return z
+end
 
-# 2 species -> fo, fa
+function foo(x::_T, y::_S)  where {_T<:Number,_S<:Number}
+    z = x + 3*y
+    return z
+end
