a4d919728874f6fbcceb43f57d55b3baba8b9e81
Jamison Assunção <jamison.assuncao@gmail.com>
2023-09-07 17:19:26 +0200

    essentially bootstraping the activity function

--- gcalc.jl
+++ gcalc.jl
@@ -3,28 +3,24 @@ using CairoMakie
 include("functions.jl")
 
 function main()
-    message("start")
 
     # user input
     pressure, temperature = 1_000.0, 1_000.0
 
+    message("start", [pressure, temperature])
+
     # model_names = ["C2/c pyroxene"]
-    # comp = make_comp(Dict("MGO" => 2.0, "FEO" => 2.0)) # C2/c pyroxene 
     # species_fractions = [[0.5, 0.5]] # C2/c pyroxene
 
-    # model_names = ["olivine"]
-    # comp = make_comp(Dict("FEO" => 1.0, "MGO" => 1.0)) # olivine
-    # species_fractions = [[0.5, 0.5]] # olivine
+    model_names = ["olivine"]
+    species_fractions = [[0.5, 0.5]] # olivine
 
     # model_names = ["perovskite"]
-    # comp = make_comp(Dict("FEO" => 1.0, "MGO" => 1.0)) # peroviskite
     # species_fractions = [[0.3, 0.3, 0.4]] # peroviskite
 
-    model_names = ["spinel"]
-    # comp = make_comp(Dict("AL2O3" => 1.0, "MGO" => 1.0)) # 100% spinel
+    # model_names = ["spinel"]
     # species_fractions = [[1.0, 0.0]] # 100% spinel
-    comp = make_comp(Dict("AL2O3" => 2.0, "FEO" => 1.0, "MGO" => 1.0)) # spinel
-    species_fractions = [[0.5, 0.5]] # spinel
+    # species_fractions = [[0.5, 0.5]] # spinel
     
 
     # read datasets
@@ -33,8 +29,8 @@ function main()
     # println(models)
 
     # calculate gibbs free energy
-    gibbs = gcalc(pressure, temperature, comp, models, species_fractions) 
-    # gibbs = span_gcalc(10, pressure, temperature, comp, models)
+    gibbs = gcalc(pressure, temperature, models, species_fractions) 
+    # gibbs = span_gcalc(10, pressure, temperature, models)
 
     return gibbs
 end
